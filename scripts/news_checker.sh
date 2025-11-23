1d29a2f1a4c47ca9f82a642832e27baabf61cb6a
Sarah Bitencourt <sarahcristinabitencourt17@gmail.com>
2025-11-23 20:43:27 -0300

    feat(script): enhance script robustness and organization
    - adds set -o directives (nounset, errexit, pipefail) for fail-fast behavior
    - organizes variables into clear, separated sections (settings, directory, other variables, and main execution)

--- scripts/news_checker.sh
+++ scripts/news_checker.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-# --- Variables ---
+# ==============================================================================
+# SCRIPT SETTINGS
+# ==============================================================================
+set -o nounset
+set -o errexit
+set -o pipefail
 
+# ==============================================================================
+# DIRECTORY VARIABLES
+# ==============================================================================
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 BASE_DIR="$(dirname -- "${SCRIPT_DIR}")"
 CONFIG_DIR="$BASE_DIR/config"
-NEWS_API_KEY=$(cat "$CONFIG_DIR/.env" | grep NEWS_API_KEY | grep -oP '=\K\w+')
 
-# --- Main Script ---
-curl "https://newsapi.org/v2/everything?q=bitcoin&apiKey=$NEWS_API_KEY"
+# ==============================================================================
+# CONFIG OTHER VARIABLES
+# ==============================================================================
+NEWS_API_KEY=$(cat "$CONFIG_DIR/.env" | grep NEWS_API_KEY | grep -oP '=\K\w+')
+QUERY="(Trump or US GOVERN) AND (crypto OR cryptocurrency OR bitcoin)"
+
+# ==============================================================================
+# MAIN EXECUTION
+# ==============================================================================
+curl "https://newsapi.org/v2/everything?q=$QUERY&apiKey=$NEWS_API_KEY"
