afa02b0edef7500491e869a9a8097e9d0d1df3ce
Sarah Bitencourt <sarahcristinabitencourt17@gmail.com>
2025-11-23 21:03:01 -0300

    feat(data): implement data storage structure and timestamp logic
    - creates DATA_DIR and TIMESTAMP variables for file naming
    - establishes persistent data storage structure ('/data/cleaned', '/data/raw')

--- scripts/news_checker.sh
+++ scripts/news_checker.sh
@@ -13,14 +13,16 @@ set -o pipefail
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 BASE_DIR="$(dirname -- "${SCRIPT_DIR}")"
 CONFIG_DIR="$BASE_DIR/config"
+DATA_DIR="$BASE_DIR/data"
 
 # ==============================================================================
 # CONFIG OTHER VARIABLES
 # ==============================================================================
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 NEWS_API_KEY=$(cat "$CONFIG_DIR/.env" | grep NEWS_API_KEY | grep -oP '=\K\w+')
 QUERY="(Trump or US GOVERN) AND (crypto OR cryptocurrency OR bitcoin)"
 
 # ==============================================================================
 # MAIN EXECUTION
 # ==============================================================================
-curl "https://newsapi.org/v2/everything?q=$QUERY&apiKey=$NEWS_API_KEY"
+curl "https://newsapi.org/v2/everything?q=$QUERY&apiKey=$NEWS_API_KEY" >> "$date '+%Y%m%d%H%M%S'"
