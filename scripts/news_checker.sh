b377b6cba2f7571dedf982d03bb2afc216007431
Sarah Bitencourt <sarahcristinabitencourt17@gmail.com>
2025-11-23 21:48:35 -0300

    feat(logging): implement full script output redirection via tee
    - uses 'exec' to redirect all subsequent output (stdout) to $LOG_FILE
    - uses 'exec 2>' to redirect all errors (stderr) to $ERR_FILE
    - adds '/log/' directory to .gitignore to prevent tracking runtime logs

--- scripts/news_checker.sh
+++ scripts/news_checker.sh
@@ -16,14 +16,23 @@ CONFIG_DIR="$BASE_DIR/config"
 DATA_DIR="$BASE_DIR/data"
 RAW_DATA_DIR="$DATA_DIR/raw"
 CLEANED_DATA_DIR="$DATA_DIR/cleaned"
+LOG_DIR="$BASE_DIR/log"
 
 # ==============================================================================
 # CONFIG OTHER VARIABLES
 # ==============================================================================
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+LOG_FILE="${LOG_DIR}/${TIMESTAMP}.log"
+ERR_FILE="${LOG_DIR}/${TIMESTAMP}.err"
 NEWS_API_KEY=$(cat "$CONFIG_DIR/.env" | grep NEWS_API_KEY | grep -oP '=\K\w+')
 QUERY="(Trump or US GOVERN) AND (crypto OR cryptocurrency OR bitcoin)"
 API_BASE_URL="https://newsapi.org/v2/everything?"
+
+# ==============================================================================
+# REDIRECT OUTPUTS TO LOG FILES
+# ==============================================================================
+exec > >(tee -a "${LOG_FILE}")
+exec 2> >(tee -a "${ERR_FILE}" >&2)
 
 # ==============================================================================
 # MAIN EXECUTION
